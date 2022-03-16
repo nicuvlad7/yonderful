9e8bf350f3cd9763ccac9b169e195dcd0ba351a3
Andy <tanaseandrei3011@gmail.com>
2022-03-16 11:58:10 +0200

    added some comments here and there

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -8,6 +8,10 @@ namespace YonderfulApi.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        /// role == 0 - user
+        /// role == 1 - admin
+        /// by default 0
         public int Role { get; set; } = 0;
         public string Email { get; set; }
         public string Password { get; set; }
