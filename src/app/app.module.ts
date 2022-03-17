1d32166606d1b7f865b63dccfba6e3a9cc57d606
Andrei Buiciuc <abuiciuc33@gmail.com>
2022-03-17 11:00:56 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { LoginCardComponent } from './components/login-card/login-card.component';
 import { EventListPageComponent } from './components/event-list-page/event-list-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterCardComponent } from './components/register-card/register-card.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { ButtonComponent } from './components/button/button.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { MaterialModules } from './modules/material.module';
 
 @NgModule({
   declarations: [
@@ -23,8 +20,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     EventDetailsComponent,
     LoginCardComponent,
     EventListPageComponent,
-    RegisterCardComponent,
-    ButtonComponent
+    RegisterCardComponent
   ],
   imports: [
     BrowserModule,
@@ -32,13 +28,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatSnackBarModule
+    MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register-card/register-card.component.ts
+++ src/app/components/register-card/register-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RegisterService } from 'src/app/services/register-service';
 import { User } from 'src/app/models/user';
@@ -10,9 +10,7 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./register-card.component.scss']
 })
 export class RegisterCardComponent implements OnInit {
-  registerNameControl!: FormControl;
-  registerEmailControl!: FormControl;
-  registerPasswordControl!: FormControl;
+  registerForm!: FormGroup;
 
   isInputValid: boolean = false;
   registrationStatus: boolean = false;
@@ -29,25 +27,32 @@ export class RegisterCardComponent implements OnInit {
   }
 
   initFormControls(): void {
-    this.registerNameControl = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]);
-    this.registerEmailControl = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+\\.[a-zA-Z]+@tss-yonder\\.com')]);
-    this.registerPasswordControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+    this.registerForm = new FormGroup({
+      registerNameControl: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]+')]),
+      registerEmailControl: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+\\.[a-zA-Z]+@tss-yonder\\.com')]),
+      registerPasswordControl: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+
   }
 
   isRegisterFormCompleted(): boolean {
-    return !(this.registerNameControl.value === '' || this.registerEmailControl.value === '' || this.registerPasswordControl.value === '')
+    return this.registerForm.touched;
   }
 
   isRegisterFormValid(): boolean {
-    return this.registerNameControl.valid && this.registerEmailControl.valid && this.registerPasswordControl.valid;
+    return this.registerForm.valid;
   }
 
   isRegisterFormDone(): boolean {
     return this.isRegisterFormCompleted() && this.isRegisterFormValid();
   }
 
-  onClick(): void {
-    const user: User = {name: this.registerNameControl.value, email: this.registerEmailControl.value, password: this.registerPasswordControl.value};
+  onRegisterUser(): void {
+    const user: User = {
+      name: this.registerForm.get('registerNameControl')!.value, 
+      email: this.registerForm.get('registerEmailControl')!.value, 
+      password: this.registerForm.get('registerPasswordControl')!.value
+    };
 
     this.registerService.register(user).subscribe({
       next: (data: User) => {
