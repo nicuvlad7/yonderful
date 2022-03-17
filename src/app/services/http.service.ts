c8ff8a5aa6050b68d4983904f5a387cc1cec57e6
Andrei Buiciuc <abuiciuc33@gmail.com>
2022-03-17 17:10:56 +0200

    commit
    asda

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -56,7 +56,7 @@ export class HttpService {
   }
 
   private getRequestUrl(endpoint: string): string {
-    return environment.apiUrl + endpoint;
+    return environment.mockUrl + endpoint;
   }
 
   handleHttpErrorResponse(error: HttpErrorResponse): Observable<any> {
