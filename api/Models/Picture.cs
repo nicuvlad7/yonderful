a089759038ced4742d5c6bae380dcb42a54ebd31
Tasadan Filip <tasadan.filip@gmail.com>
2022-03-21 10:43:50 +0200

    Save pictures as Base64 string, not as byes content

--- api/Models/Picture.cs
+++ api/Models/Picture.cs
@@ -9,6 +9,6 @@ namespace YonderfulApi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string FileType { get; set; }
-        public byte[] Content { get; set; }
+        public string Content { get; set; }
     }
 }
