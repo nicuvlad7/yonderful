a5244ce4512228e933d895e83fdafdbc0f1b9f40
Tasadan Filip <tasadan.filip@gmail.com>
2022-03-23 11:34:04 +0200

    Sort service done

---
+++ src/app/services/sort-helper-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Type } from '@angular/core';
+import { Sort } from '../models/sort';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SortHelperServiceService {
+
+  constructor() { }
+
+  sort<Type>(sort: Sort, data: Array<any>): Type[] {
+    const isAsc = sort.direction === 'asc';
+    return data.sort((a, b) => {
+        return this.compare(a[sort.field], b[sort.field], isAsc);
+    });
+  }
+
+  compare(a: number | string, b: number | string, isAsc: boolean): number {
+	  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+}
