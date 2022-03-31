4c4d9f08848f9630a67c254cc195ceb791602a20
Andy <97806404+silverbabyyyy@users.noreply.github.com>
2022-03-31 12:09:01 +0300

    formatted a file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,19 +10,19 @@ import { AdminGuard, AuthGuard, UserGuard } from './helpers/auth.guard';
 import { RouteValues } from './models/constants';
 
 const routes: Routes = [
-  { path: RouteValues.LOGIN, component: LoginCardComponent },
-  { path: RouteValues.REGISTER, component: RegisterCardComponent },
-  { path: RouteValues.ADMINISTRATE_CATEGORIES, component: CategoriesTableComponent, canActivate: [AuthGuard, AdminGuard]},
-  { path: RouteValues.CREATE_EDIT_EVENT, component: CreateEditEventPageComponent, canActivate: [AuthGuard, UserGuard] },
-  { path : RouteValues.CATEGORY_NEW, component: CategoryCardComponent, canActivate: [AuthGuard, AdminGuard]},
-  { path : RouteValues.CATEGORY_ID,component: CategoryCardComponent, canActivate: [AuthGuard, AdminGuard]},
-  { path: RouteValues.EVENT_DETAILS_ID, component: EventPageComponent, canActivate: [AuthGuard, UserGuard]},
-  { path: RouteValues.DEFAULT, redirectTo: RouteValues.LOGIN, pathMatch: 'full'}
+    { path: RouteValues.LOGIN, component: LoginCardComponent },
+    { path: RouteValues.REGISTER, component: RegisterCardComponent },
+    { path: RouteValues.ADMINISTRATE_CATEGORIES, component: CategoriesTableComponent, canActivate: [AuthGuard, AdminGuard] },
+    { path: RouteValues.CREATE_EDIT_EVENT, component: CreateEditEventPageComponent, canActivate: [AuthGuard, UserGuard] },
+    { path: RouteValues.CATEGORY_NEW, component: CategoryCardComponent, canActivate: [AuthGuard, AdminGuard] },
+    { path: RouteValues.CATEGORY_ID, component: CategoryCardComponent, canActivate: [AuthGuard, AdminGuard] },
+    { path: RouteValues.EVENT_DETAILS_ID, component: EventPageComponent, canActivate: [AuthGuard, UserGuard] },
+    { path: RouteValues.DEFAULT, redirectTo: RouteValues.LOGIN, pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 
 export class AppRoutingModule { }
