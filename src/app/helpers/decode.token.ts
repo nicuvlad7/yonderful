ce5434b66fb0ba60ba99fe88c804879baa6608a5
Tasadan Filip <tasadan.filip@gmail.com>
2022-03-31 12:54:24 +0300

    get current user id problem resolved

---
+++ src/app/helpers/decode.token.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({ providedIn: 'root' })
+export class DecodeToken {
+    token = JSON.parse(localStorage.getItem('currentUser')).token;
+    tokenInfo: any;
+
+    constructor() {
+        this.initialiseLocalStorage();
+    }
+
+    getDecodedAccessToken(token: string): any {
+        try {
+        return jwt_decode(token);
+        } catch(Error) {
+        return null;
+        }
+    }
+
+    getCurrentUserId() {
+        return this.tokenInfo.UserID;
+    }
+
+    initialiseLocalStorage() {
+        this.tokenInfo = this.getDecodedAccessToken(this.token); // decode token
+    }
+}
