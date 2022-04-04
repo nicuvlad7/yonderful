1a901a4e3065b8a0c8c321cc860bafcc73eb7817
dennis <talosdennisdaniel@gmail.com>
2022-04-04 15:52:51 +0300

    [FE] finished the layout, and started on implementing functionalities

--- src/app/components/generic-filter/generic-filter.component.ts
+++ src/app/components/generic-filter/generic-filter.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
-  selector: 'app-generic-filter',
-  templateUrl: './generic-filter.component.html',
-  styleUrls: ['./generic-filter.component.scss']
+	selector: 'app-generic-filter',
+	templateUrl: './generic-filter.component.html',
+	styleUrls: ['./generic-filter.component.scss'],
 })
 export class GenericFilterComponent implements OnInit {
+	@Output() isAscending = new EventEmitter<Boolean>();
+  @Output() sortBy = new EventEmitter<string>();
+  @Output() startDate = new EventEmitter<Date>();
+  @Output() endDate = new EventEmitter<Date>();
+  @Output() categories = new EventEmitter<string[]>();
+  @Output() hiddenIfFee = new EventEmitter<boolean>();
+  @Output() hiddenIfStarted = new EventEmitter<boolean>();
+  
+  sortBy2:string;
+  isAscending2:boolean=false;
+	constructor(private categoryService: CategoryService) {}
 
-  constructor() { }
-  isAscending:boolean=false;
-  ngOnInit(): void {
+	ngOnInit(): void {
+
   }
-
+  emitSortBy(){
+    this.sortBy.emit(this.sortBy2);
+  }
 }
