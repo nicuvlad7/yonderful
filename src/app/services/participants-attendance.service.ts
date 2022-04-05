6c96b9dd03d1c079c409177aa0de92d431c61d02
silverbabyyyy <tanaseandrei3011@gmail.com>
2022-04-05 15:05:59 +0300

    solved comments and added provisional number of participants

--- src/app/services/participants-attendance.service.ts
+++ src/app/services/participants-attendance.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { RouteEndpoints } from '../models/constants';
 import { User } from '../models/user';
 import { HttpService } from './http.service';
 
 @Injectable({
-	providedIn: 'root',
+    providedIn: 'root',
 })
 export class ParticipantsAttendanceService {
-	constructor(private httpService: HttpService) {}
+    constructor(private httpService: HttpService) { }
 
     //to-do:
     //once the attendance table is merged into master,come back here and configure the right links,
     //data type for the participants/attendance
-    
-	deleteParticipant(id: number): Observable<User> {
-		return this.httpService
-			.delete<User>(id, `Attendance?attendanceId=`)
-			.pipe(catchError(this.httpService.handleHttpErrorResponse));
-	}
 
+    deleteParticipant(id: number): Observable<User> {
+        return this.httpService
+            .delete<User>(id, `Attendance?attendanceId=`)
+            .pipe(catchError(this.httpService.handleHttpErrorResponse));
+    }
 }
